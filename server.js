131813a5e17f91db0900fda2fbcf066ec82da37e
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-01-22 17:25:41 -0500

    changing code to hopefully work

--- server.js
+++ server.js
@@ -1,38 +1,58 @@
-// ___________Definitely referencing mini project for help!!! This is so hard lol _______
+// // ___________Definitely referencing mini project for help!!! This is so hard lol _______
 
-const express = require('express');
-const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+// const express = require('express');
+// const path = require('path');
+// const { clog } = require('./middleware/clog');
+// const api = require('./routes/index.js');
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
-const app = express();
+// const app = express();
 
-// Import custom middleware, "cLog"
-app.use(clog);
+// // Import custom middleware, "cLog"
+// app.use(clog);
 
-// Middleware for parsing JSON and urlencoded form data
+// // Middleware for parsing JSON and urlencoded form data
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
+// app.use(express.static('public'));
+
+// // // Code from mini project i'm referencing to get routes
+// // app.get('/', (req, res) =>
+// //   res.sendFile(path.join(__dirname, '/public/index.html'))
+// // );
+
+// // GET Route for notes page
+// app.get('/notes', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/notes.html'))
+// );
+
+// // Wildcard route to direct users to index.html
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/index.html'))
+// );
+
+// app.listen(PORT, () =>
+//   console.log(`App listening at http://localhost:${PORT} 🚀`)
+// );
+//call all dependencies
+var express = require("express");
+var apiroutes = require("./Develop/routes/apiroutes");
+var htmlroutes = require("./Develop/routes/htmlroutes");
+
+var app = express();
+var PORT  = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use(express.static("public"));
+app.use("/api", apiroutes);
+app.use("/",htmlroutes);
 
-app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-// Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
