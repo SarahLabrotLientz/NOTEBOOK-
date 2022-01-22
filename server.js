61b56c7d585a3b7ced97487f345998d0ee7b62be
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-01-22 15:55:16 -0500

    reworking server js with mini project from class

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
+// ___________Definitely referencing mini project for help!!! This is so hard lol _______
+
 const express = require('express');
-const app = express();
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
+
 const PORT = process.env.PORT || 3001;
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const app = express();
 
-// parse POST data
-app.use(express.urlencoded({ extended: true }));
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
