76aa6dbface3484b1632356c536a90536f22327f
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-01-23 14:22:28 -0500

    got help from tutor reworking all my code to one file what a nightmare

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+// const express = require('express');
+// const path = require('path');
+// const { clog } = require('./middleware/clog');
+// const api = require('./routes/index.js');
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+// // Import custom middleware, "cLog"----Ask Rich how he used this, looks cool
+// app.use(clog);
+
+// // Middleware for parsing JSON and urlencoded form data
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
